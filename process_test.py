3c36e230173c747ccd0270dc6419f6ff1d12f824
Youngkl0726 <yklguge@gmail.com>
2017-11-16 21:52:18 +0800

    update

--- process_test.py
+++ process_test.py
@@ -7,7 +7,7 @@ total_num = 0
 for line in csv_file:
     total_num += 1
     # print line
-    txt_file.write('{:0}.png'.format(int(line[1])) + ' ' + '0' + '\n')
+    txt_file.write('{:0>11d}.png'.format(int(line[1])) + ' ' + '0' + '\n')
 
 print total_num
 txt_file.close()
