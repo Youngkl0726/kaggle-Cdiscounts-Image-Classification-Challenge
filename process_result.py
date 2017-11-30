deadaa4ee56517c18db99b76963435ca60f4bbea
Youngkl0726 <yklguge@gmail.com>
2017-11-30 21:18:31 +0800

    add model

--- process_result.py
+++ process_result.py
@@ -3,8 +3,8 @@ import csv
 # total = 0
 # txt_file = open('result.txt', 'wb')
 # for i in xrange(36):
-#     npy_name = './kaggle_result1/n1_result/prob_{:0}.npy'.format(i)
-#     # print npy_name
+#     npy_name = './n1/prob_{:0}.npy'.format(i)
+#     print npy_name
 #     npy = np.load(npy_name)
 #     len = npy.shape[0]
 #     # print npy.shape[0]
